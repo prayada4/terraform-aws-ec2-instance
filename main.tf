e751307cc7ae00979b6d81ea6048cd226a615561
prayada4 <121497330+prayada4@users.noreply.github.com>
2023-01-09 21:20:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
+resource "aws_network_interface" "this" {
+  subnet_id   = var.my_subnet.id
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+
 resource "aws_instance" "this" {
   ami           = var.ami
   instance_type = var.instance_type
+  
+  network_interface {
+    network_interface_id = aws_network_interface.this.id
+    device_index         = 0
+  }
 }
